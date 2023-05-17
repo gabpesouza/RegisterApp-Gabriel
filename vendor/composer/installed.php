1c57536d7775662e7966402c428691753bdd1885
gabrielpsouza <gabriel.psouza1997@outlook.com>
2023-05-17 20:23:10 -0300

    projeto criado

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'teste/2utec',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da50e48d88f96334ab50353756a328491853f34d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'league/plates' => array(
+            'pretty_version' => 'v4.0.0-alpha',
+            'version' => '4.0.0.0-alpha',
+            'reference' => 'f53f4c1fa4bf307d0f3858a348172ff4faf7669a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'teste/2utec' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da50e48d88f96334ab50353756a328491853f34d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
