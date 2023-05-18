e7c4213d529398bc97d6476574864c18e6b1d2c6
gabrielpsouza <gabriel.psouza1997@outlook.com>
2023-05-17 21:30:58 -0300

    banco de dados e tabelas criados usando MySql

---
+++ projeto_cadastro.sql
@@ -0,0 +1,126 @@
+
+/**DUMP DO SQL**/
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2023 at 02:28 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projeto_cadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_enderecos`
+--
+
+CREATE TABLE `tb_enderecos` (
+  `id_endereco` int(11) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `tipo_logradouro` varchar(20) NOT NULL,
+  `logradouro` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_usuarios`
+--
+
+CREATE TABLE `tb_usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_usuarios_enderecos`
+--
+
+CREATE TABLE `tb_usuarios_enderecos` (
+  `id_registro` int(11) NOT NULL,
+  `id_endereco` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_enderecos`
+--
+ALTER TABLE `tb_enderecos`
+  ADD PRIMARY KEY (`id_endereco`);
+
+--
+-- Indexes for table `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indexes for table `tb_usuarios_enderecos`
+--
+ALTER TABLE `tb_usuarios_enderecos`
+  ADD PRIMARY KEY (`id_registro`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_endereco` (`id_endereco`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_enderecos`
+--
+ALTER TABLE `tb_enderecos`
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_usuarios_enderecos`
+--
+ALTER TABLE `tb_usuarios_enderecos`
+  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_usuarios_enderecos`
+--
+ALTER TABLE `tb_usuarios_enderecos`
+  ADD CONSTRAINT `tb_usuarios_enderecos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id_usuario`),
+  ADD CONSTRAINT `tb_usuarios_enderecos_ibfk_2` FOREIGN KEY (`id_endereco`) REFERENCES `tb_enderecos` (`id_endereco`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
