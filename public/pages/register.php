820ea096c61b07b3be79398bf23af2680b14f556
gabrielpsouza <gabriel.psouza1997@outlook.com>
2023-05-20 13:25:08 -0300

    Front end da pagina register finalizado, configuracoes de conexao com o banco definidas em config.php

--- public/pages/register.php
+++ public/pages/register.php
@@ -1,51 +1,56 @@
-<div class="col-lg-6 col-xl-6 card-wrapper">
-    <div class="card-register">
-        <div class="card-center">
-            <div class="card-header"><span>Registro</span></div>
-            <div class="d-flex flex-wrap align-items-center justify-content-between" style="width: 100%">
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="nome">Nome:</label>
-                    <input class="form-control" id="nome" name="nome" type="text">
+<form method="POST" action="success.php">
+    <div class="col-lg-6 col-xl-6 card-wrapper">
+        <div class="card-register">
+            <div class="card-center">
+                <div class="card-header"><span>Registro</span></div>
+                <div class="d-flex flex-wrap align-items-center mt-2 justify-content-between w-100">
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="nome">Nome:</label>
+                        <input class="form-control" id="nome" name="nome" type="text">
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="email">Email:</label>
+                        <input class="form-control" id="email" name="email" type="text">
+                    </div>
                 </div>
-                <div class="col-1"></div>
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="email">Email:</label>
-                    <input class="form-control" id="email" name="email" type="text">
+                <div class="d-flex flex-wrap align-items-center mt-2 justify-content-between w-100">
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="cep">Cep:</label>
+                        <input class="form-control" id="cep" name="cep" type="text">
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="tipo_logradouro">Tipo logradouro:</label>
+                        <input class="form-control" id="tipo_logradouro" name="tipo_logradouro" type="text">
+                    </div>
                 </div>
-            </div>
-            <div class="d-flex flex-wrap align-items-center justify-content-between" style="width: 100%">
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="cep">Cep:</label>
-                    <input class="form-control" id="cep" name="cep" type="text">
+                <div class="d-flex flex-wrap align-items-center mt-2 justify-content-between w-100">
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="estado">Estado:</label>
+                        <input class="form-control" id="estado" name="estado" type="text">
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="cidade">Cidade:</label>
+                        <input class="form-control" id="cidade" name="cidade" type="text">
+                    </div>
                 </div>
-                <div class="col-1"></div>
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="tipo_logradouro">Tipo logradouro:</label>
-                    <input class="form-control" id="tipo_logradouro" name="tipo_logradouro" type="text">
+                <div class="d-flex flex-wrap align-items-center mt-2 justify-content-between w-100">
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="estado">Logradouro:</label>
+                        <input class="form-control" id="estado" name="estado" type="text">
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <label class="form-label" for="cidade">Bairro:</label>
+                        <input class="form-control" id="cidade" name="cidade" type="text">
+                    </div>
                 </div>
-            </div>
-            <div class="d-flex flex-wrap align-items-center justify-content-between" style="width: 100%">
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="estado">Estado:</label>
-                    <input class="form-control" id="estado" name="estado" type="text">
-                </div>
-                <div class="col-1"></div>
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="cidade">Cidade:</label>
-                    <input class="form-control" id="cidade" name="cidade" type="text">
-                </div>
-            </div>
-            <div class="d-flex flex-wrap align-items-center justify-content-between" style="width: 100%">
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="estado">Logradouro:</label>
-                    <input class="form-control" id="estado" name="estado" type="text">
-                </div>
-                <div class="col-1"></div>
-                <div class="form-group col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                    <label class="form-label" for="cidade">Bairro:</label>
-                    <input class="form-control" id="cidade" name="cidade" type="text">
+                <div class="d-flex  mt-4 justify-content-center w-100">
+                    <button class="btn btn-primary btn-block">Cadastrar</button>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</form>
--- source/Support/Config.php
+++ source/Support/Config.php
@@ -1,1 +1,8 @@
 <?php
+
+/** DATABASE CONFIG**/
+define("DB_HOST","localhost");
+define("DB_NAME","projeto_cadastro");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_CHARSET","utf 8");
